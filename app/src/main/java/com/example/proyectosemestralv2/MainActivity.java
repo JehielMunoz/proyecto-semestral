62eb4449475b45d3f684dfc0a7c47d8a7dfa135a
JehielMunoz <jehiel.munoz2015@alu.uct.cl>
2021-04-30 22:05:33 -0400

    Comentario de prueba.

--- app/src/main/java/com/example/proyectosemestralv2/MainActivity.java
+++ app/src/main/java/com/example/proyectosemestralv2/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //comentario de prueba v2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
