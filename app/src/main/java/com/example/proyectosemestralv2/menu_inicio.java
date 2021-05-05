4c5173f897ce7963eb85b79c0d194eef9c0563ab
Cristobal Astudillo <labotsirc.1997@gmail.com>
2021-05-05 16:21:33 -0400

    Se integra la pantalla de modificacion de proveedores, creacion de usuario y se crea una base para listar los proveedores registrados.

--- app/src/main/java/com/example/proyectosemestralv2/menu_inicio.java
+++ app/src/main/java/com/example/proyectosemestralv2/menu_inicio.java
@@ -1,8 +1,14 @@
 package com.example.proyectosemestralv2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class menu_inicio extends AppCompatActivity {
 
@@ -11,4 +17,5 @@ public class menu_inicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_inicio);
     }
+
 }
