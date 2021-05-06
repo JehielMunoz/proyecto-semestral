a03f96f6bdffc252ceb810a8bda2937797c6665a
felipe_N <felipe.novoa2020@twk.cl>
2021-05-06 10:18:03 -0400

    ingreso de items

---
+++ app/src/main/java/com/example/proyectosemestralv2/add_item.java
@@ -0,0 +1,14 @@
+package com.example.proyectosemestralv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
