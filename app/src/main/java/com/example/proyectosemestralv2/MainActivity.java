db3f5b0ce6e76c1b0211004caf786ec0e187db6c
felipe_N <felipe.novoa2020@twk.cl>
2021-05-07 13:30:21 -0400

    ... 07-05

--- app/src/main/java/com/example/proyectosemestralv2/MainActivity.java
+++ app/src/main/java/com/example/proyectosemestralv2/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Hola soy Felipe, y yo soy jehiel.
+
     }
 }
--- app/src/main/java/com/example/proyectosemestralv2/add_item.java
+++ app/src/main/java/com/example/proyectosemestralv2/add_item.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class add_item extends AppCompatActivity {
 
     @Override
@@ -11,4 +12,7 @@ public class add_item extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
     }
+
+
+
 }
