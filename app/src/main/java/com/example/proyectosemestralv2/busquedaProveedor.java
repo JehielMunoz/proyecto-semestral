91ca2edb75c30d60893873f3cf7d68afe07b1494
Cristobal Astudillo <labotsirc.1997@gmail.com>
2021-05-10 17:16:54 -0400

    Correcciones extra busqueda proveedor

--- app/src/main/java/com/example/proyectosemestralv2/busquedaProveedor.java
+++ app/src/main/java/com/example/proyectosemestralv2/busquedaProveedor.java
@@ -2,13 +2,36 @@ package com.example.proyectosemestralv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class busquedaProveedor extends AppCompatActivity {
+public class busquedaProveedor extends AppCompatActivity implements View.OnClickListener{
+
+    Button retornoBtn, buscarBtn, guardarCambiosBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busqueda_proveedor);
+
+        buscarBtn = (Button)findViewById(R.id.buscarButton);
+        retornoBtn = (Button)findViewById(R.id.retornoButton);
+        guardarCambiosBtn = (Button)findViewById(R.id.guardarCambiosButton);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buscarButton:
+                break;
+            case R.id.retornoButton:
+                Intent intentRB = new Intent(busquedaProveedor.this, menu_inicio.class);
+                startActivity(intentRB);
+            case R.id.guardarCambiosButton:
+                break;
+        }
     }
 }
