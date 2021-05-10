94e198542e511a34cd824f26a44e8ef4f566026d
Cristobal Astudillo <labotsirc.1997@gmail.com>
2021-05-10 15:34:24 -0400

    Se finalizan las pantallas de proveedores, tanto el registro inicial de proveedores como la busqueda y modifcacion de los existentes en sistema.

--- app/src/main/java/com/example/proyectosemestralv2/busquedaProveedor.java
+++ app/src/main/java/com/example/proyectosemestralv2/busquedaProveedor.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class listado_proveedores extends AppCompatActivity {
+public class busquedaProveedor extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listado_proveedores);
+        setContentView(R.layout.activity_busqueda_proveedor);
     }
 }
