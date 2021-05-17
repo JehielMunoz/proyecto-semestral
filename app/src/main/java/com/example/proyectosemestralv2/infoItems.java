cafbed3d8d46f720093b9042ed52c6bf2dda67d5
felipe_N <felipe.novoa2020@twk.cl>
2021-05-16 21:57:54 -0400

    informacion de intem pantalla y boton agregado v2.0

--- app/src/main/java/com/example/proyectosemestralv2/infoItems.java
+++ app/src/main/java/com/example/proyectosemestralv2/infoItems.java
@@ -2,20 +2,33 @@ package com.example.proyectosemestralv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class infoItems extends AppCompatActivity {
-
+         Button btnvolver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_items);
 
+        btnvolver = (Button)findViewById(R.id.btnvolver);
 
+        btnvolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(new Intent(infoItems.this,menuInicio.class));
+                startActivity(intent);
+            }
+        });
 
 
 
 
 
     }
+    public void onBackPressed(){
+        }
 }
