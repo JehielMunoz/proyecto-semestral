386d303af13916993754ff9396f169e1b76c702d
felipe_N <felipe.novoa2020@twk.cl>
2021-05-28 20:46:34 -0400

    PANTALLA BusquedaAvanzada

---
+++ app/src/main/java/com/example/proyectosemestralv2/BusquedaAvanzada.java
@@ -0,0 +1,14 @@
+package com.example.proyectosemestralv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusquedaAvanzada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busqueda_avanzada);
+    }
+}
