bd7448114334d94a6487eea4c66132a088532ec2
JehielMunoz <jehiel.munoz2015@alu.uct.cl>
2021-05-31 11:46:47 -0400

    Exportación a excel v1.9.5: Exportación funcionando con formato original y por ubicacion(hay que cambiar el spinner a edittext para que funcione esto), testeado desde el registro 1 al 100.

--- app/src/main/java/com/example/proyectosemestralv2/archivoExporExcel.java
+++ app/src/main/java/com/example/proyectosemestralv2/archivoExporExcel.java
@@ -8,8 +8,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -20,36 +23,63 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jxl.write.WritableWorkbook;
 import jxl.write.WriteException;
 
-public class archivoExporExcel extends AppCompatActivity implements View.OnClickListener {
+public class archivoExporExcel extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     Button guardarCambios, btnAtras, export, export_ubicacion;
+    Spinner spinner;
     daoExportExcel daoExport;
     daoEspecie daoEspecie;
     private DatabaseReference mDatabase;
     String urlDb = "https://proyectoi-invedu-default-rtdb.firebaseio.com/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archivo_export_excel);
 
-        export =        findViewById(R.id.btnExportGralExcel);
-        export_ubicacion= findViewById(R.id.btnExportUbiExcel);
-        guardarCambios= findViewById(R.id.guardarCambiosbtn);
-        btnAtras =      findViewById(R.id.btnAtras);
+        export = findViewById(R.id.btnExportGralExcel);
+        export_ubicacion = findViewById(R.id.btnExportUbiExcel);
+        guardarCambios = findViewById(R.id.guardarCambiosbtn);
+        btnAtras = findViewById(R.id.btnAtras);
+        Spinner spinner = (Spinner) findViewById(R.id.exUbicacionExport);
 
         mDatabase = FirebaseDatabase.getInstance(urlDb).getReference();
 
-        daoExport   = new daoExportExcel();
-        daoEspecie  = new daoEspecie(this);
+        daoExport = new daoExportExcel();
+        daoEspecie = new daoEspecie(this);
 
+        //UBICACIONES//
+        ArrayList<String> ubicaciones = daoExport.getUbi(mDatabase);
+
+        ArrayAdapter<CharSequence> spinnerArrayAdapter = new ArrayAdapter
+                (this, android.R.layout.simple_spinner_item, ubicaciones);
+        spinner.setAdapter(spinnerArrayAdapter);
+        //spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spinner.setSelection(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        //UBICACIONES//
+
+        spinner.setOnItemSelectedListener(this);
         export.setOnClickListener(this);
+        export_ubicacion.setOnClickListener(this);
         btnAtras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(new Intent(archivoExporExcel.this,menuInicio.class));
+                Intent intent = new Intent(new Intent(archivoExporExcel.this, menuInicio.class));
                 startActivity(intent);
             }
         });
@@ -62,11 +92,31 @@ public class archivoExporExcel extends AppCompatActivity implements View.OnClick
         switch (v.getId()) {
             case R.id.btnExportGralExcel:
                 try {
-                    daoExport.export(mDatabase,this);
+                    daoExport.export(archivoExporExcel.this, mDatabase, "");
+                    Toast.makeText(archivoExporExcel.this, "excel creado", Toast.LENGTH_LONG).show();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                Toast.makeText(this,"excel creado",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.exUbicacionExport:
+                String ubicacion = spinner.getSelectedItem().toString();
+                try {
+                    daoExport.export(archivoExporExcel.this, mDatabase, ubicacion);
+                    Toast.makeText(archivoExporExcel.this, "excel creado", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/example/proyectosemestralv2/daoExportExcel.java
+++ app/src/main/java/com/example/proyectosemestralv2/daoExportExcel.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.res.AssetManager;
 import android.os.Build;
 import android.os.Environment;
+import android.provider.ContactsContract;
 import android.widget.Toast;
 
 import com.google.firebase.database.Query;
@@ -18,10 +19,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.BufferedWriter;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -36,24 +40,36 @@ import jxl.write.Number;
 import jxl.write.biff.RowsExceededException;
 
 class daoExportExcel extends AppCompatActivity {
-
-    private Object LocalDateTime;
-
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public void export(DatabaseReference mDatabase, Context context) throws IOException {
-        //File plantilla = new File(context.getFilesDir(),"excel-base.xls");
+    public void export(Context context, DatabaseReference mDatabase, String ubicacion) throws IOException {
 
-        String dirPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath();
-        File directory = new File(dirPath);
-        if(!directory.exists()){ directory.mkdirs(); }
         AssetManager assetManager = context.getAssets();
         InputStream in = null;
         OutputStream out = null;
+        //get time
+        Calendar calendar = Calendar.getInstance();
+        String year =   String.valueOf(calendar.get(Calendar.YEAR));
+        String month =  String.valueOf(calendar.get(Calendar.MONTH)+1);
+        String day  =   String.valueOf(calendar.get(Calendar.DATE));
+        String hour =   String.valueOf(calendar.get(Calendar.HOUR));
+        String minutes= String.valueOf(calendar.get(Calendar.MINUTE));
+        String seconds= String.valueOf(calendar.get(Calendar.SECOND));
+        if(Integer.parseInt(hour)<10){      hour =      "0" + hour; }
+        if(Integer.parseInt(minutes)<10){   minutes =   "0" + minutes;}
+        if(Integer.parseInt(seconds)<10){   seconds =   "0" + seconds;}
+        String nameFormat = "EXPORT_INVENTARIO_"
+                + year + "-" + month + "-" + day + "_"
+                + hour + "-" + minutes + "-" + seconds +".xls";
+        String nFile = nameFormat;
+        String dirPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath();
+        File directory = new File(dirPath);
+        if(!directory.exists()){ directory.mkdirs(); }
 
+        //create file from template
         try {
             in = assetManager.open("excel-base.xls");
-            File outFile = new File(dirPath, "INVENTARIO-EXPORT.xls");
-            out = new FileOutputStream(outFile);
+            File outFilez = new File (dirPath, nFile);
+            out = new FileOutputStream(outFilez,false);
             copyFile(in, out);
         }catch(IOException e){
             e.printStackTrace();
@@ -68,17 +84,13 @@ class daoExportExcel extends AppCompatActivity {
                 catch (IOException e){e.printStackTrace();}
             }
         }
-        //create directory if not exist
-        if (!directory.isDirectory()) {
-            directory.mkdirs();
-        }
+
         try {
-            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),"INVENTARIO-EXPORT.xls");
-            //String path = Environment.getExternalStorageDirectory() + File.separator + MY_DIR_NAME + File.separator + "file.xml"
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),nFile);
+            //file.createNewFile();
             //get data
 
             //file path
-            //File file = new File(directory, csvFile);
             WorkbookSettings wbSettings = new WorkbookSettings();
             wbSettings.setLocale(new Locale(Locale.ENGLISH.getLanguage(), Locale.ENGLISH.getCountry()));
 
@@ -86,13 +98,9 @@ class daoExportExcel extends AppCompatActivity {
             WritableWorkbook workbook = Workbook.createWorkbook(file,workbookTemplate);
             workbookTemplate.close();
             WritableSheet sheetA = workbook.getSheet(0);
-            //WritableSheet sheetA = workbook.getSheet(0);
-            //workbook = Workbook.createWorkbook(file, wbSettings);
-            //Excel sheetA first sheetA
-            //WritableSheet sheetA = workbook.("sheet A", 0);
 
             // R O W S
-            //String ubicacion = ubi;
+
             Query query = mDatabase.child("data").child("especies");
             query.addValueEventListener(new ValueEventListener() {
                 @Override
@@ -100,38 +108,80 @@ class daoExportExcel extends AppCompatActivity {
                     if(dataSnapshot!=null){
                         String cont;
                         int i;
-                        for(i=1; i<100;i++) {
-                            cont = String.valueOf(i);
-                            String codigo_correlativo = String.valueOf(dataSnapshot.child(cont).child("codigo_correlativo").getValue(long.class));
-                            String especie = String.valueOf(dataSnapshot.child(cont).child("especie").getValue(String.class));
-                            String estado = String.valueOf(dataSnapshot.child(cont).child("estado").getValue(String.class));
-                            String precio_unitario = String.valueOf(dataSnapshot.child(cont).child("precio_unitario").getValue(long.class));
-                            String precio_total = String.valueOf(dataSnapshot.child(cont).child("precio_total").getValue(long.class));
-                            String fecha_recepcion = String.valueOf(dataSnapshot.child(cont).child("fecha_recepcion").getValue(String.class));
-                            String numero_factura = String.valueOf(dataSnapshot.child(cont).child("numero_factura").getValue(long.class));
-                            String rut_proveedor = String.valueOf(dataSnapshot.child(cont).child("rut_proveedor").getValue(String.class));
-                            String centro_de_costo = String.valueOf(dataSnapshot.child(cont).child("centro_de_costo").getValue(String.class));
-                            String ubicacion_actual = String.valueOf(dataSnapshot.child(cont).child("ubicacion_actual").getValue(String.class));
-                            String observaciones = String.valueOf(dataSnapshot.child(cont).child("observaciones").getValue(String.class));
+                        if(ubicacion=="") {
+                            for (i = 1; i < dataSnapshot.getChildrenCount() + 1; i++) {
+                                cont = String.valueOf(i);
+                                String codigo_correlativo = String.valueOf(dataSnapshot.child(cont).child("codigo_correlativo").getValue(long.class));
+                                String especie = String.valueOf(dataSnapshot.child(cont).child("especie").getValue(String.class));
+                                String estado = String.valueOf(dataSnapshot.child(cont).child("estado").getValue(String.class));
+                                String precio_unitario = String.valueOf(dataSnapshot.child(cont).child("precio_unitario").getValue(long.class));
+                                String precio_total = String.valueOf(dataSnapshot.child(cont).child("precio_total").getValue(long.class));
+                                String fecha_recepcion = String.valueOf(dataSnapshot.child(cont).child("fecha_recepcion").getValue(String.class));
+                                String numero_factura = String.valueOf(dataSnapshot.child(cont).child("numero_factura").getValue(long.class));
+                                String rut_proveedor = String.valueOf(dataSnapshot.child(cont).child("rut_proveedor").getValue(String.class));
+                                String centro_de_costo = String.valueOf(dataSnapshot.child(cont).child("centro_de_costo").getValue(String.class));
+                                String ubicacion_actual = String.valueOf(dataSnapshot.child(cont).child("ubicacion_actual").getValue(String.class));
+                                String observaciones = String.valueOf(dataSnapshot.child(cont).child("observaciones").getValue(String.class));
 
-                            try {
-                                sheetA.setRowView(i+13,35*20);
-                                sheetA.addCell(new Label(0, i+13, codigo_correlativo));
-                                sheetA.addCell(new Label(1, i+13, especie));
-                                sheetA.addCell(new Label(2, i+13, "1"));
-                                sheetA.addCell(new Label(3, i+13, estado));
-                                sheetA.addCell(new Label(4, i+13, precio_unitario));
-                                sheetA.addCell(new Label(5, i+13, precio_total));
-                                sheetA.addCell(new Label(6, i+13, fecha_recepcion));
-                                sheetA.addCell(new Label(7, i+13, numero_factura));
-                                sheetA.addCell(new Label(8, i+13, rut_proveedor));
-                                sheetA.addCell(new Label(9, i+13, centro_de_costo));
-                                sheetA.addCell(new Label(10, i+13, ubicacion_actual));
-                                sheetA.addCell(new Label(11, i+13, observaciones));
-                            } catch (RowsExceededException e) {
-                                e.printStackTrace();
-                            } catch (WriteException e) {
-                                e.printStackTrace();
+                                try {
+                                    sheetA.setRowView(i + 13, 30 * 20);
+                                    sheetA.addCell(new Label(0, i + 13, codigo_correlativo));
+                                    sheetA.addCell(new Label(1, i + 13, especie));
+                                    sheetA.addCell(new Label(2, i + 13, "1"));
+                                    sheetA.addCell(new Label(3, i + 13, estado));
+                                    sheetA.addCell(new Label(4, i + 13, precio_unitario));
+                                    sheetA.addCell(new Label(5, i + 13, precio_total));
+                                    sheetA.addCell(new Label(6, i + 13, fecha_recepcion));
+                                    sheetA.addCell(new Label(7, i + 13, numero_factura));
+                                    sheetA.addCell(new Label(8, i + 13, rut_proveedor));
+                                    sheetA.addCell(new Label(9, i + 13, centro_de_costo));
+                                    sheetA.addCell(new Label(10, i + 13, ubicacion_actual));
+                                    sheetA.addCell(new Label(11, i + 13, observaciones));
+                                } catch (RowsExceededException e) {
+                                    e.printStackTrace();
+                                } catch (WriteException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }else{
+                            int rCont = 13;
+                            for (i = 1; i < dataSnapshot.getChildrenCount() + 1; i++) {
+                                cont = String.valueOf(i);
+                                String aux = String.valueOf(dataSnapshot.child(cont).child("ubicacion_actual").getValue(String.class));
+                                if (aux.equals(ubicacion)) {
+                                    String codigo_correlativo = String.valueOf(dataSnapshot.child(cont).child("codigo_correlativo").getValue(long.class));
+                                    String especie = String.valueOf(dataSnapshot.child(cont).child("especie").getValue(String.class));
+                                    String estado = String.valueOf(dataSnapshot.child(cont).child("estado").getValue(String.class));
+                                    String precio_unitario = String.valueOf(dataSnapshot.child(cont).child("precio_unitario").getValue(long.class));
+                                    String precio_total = String.valueOf(dataSnapshot.child(cont).child("precio_total").getValue(long.class));
+                                    String fecha_recepcion = String.valueOf(dataSnapshot.child(cont).child("fecha_recepcion").getValue(String.class));
+                                    String numero_factura = String.valueOf(dataSnapshot.child(cont).child("numero_factura").getValue(long.class));
+                                    String rut_proveedor = String.valueOf(dataSnapshot.child(cont).child("rut_proveedor").getValue(String.class));
+                                    String centro_de_costo = String.valueOf(dataSnapshot.child(cont).child("centro_de_costo").getValue(String.class));
+                                    String ubicacion_actual = String.valueOf(dataSnapshot.child(cont).child("ubicacion_actual").getValue(String.class));
+                                    String observaciones = String.valueOf(dataSnapshot.child(cont).child("observaciones").getValue(String.class));
+
+                                    try {
+                                        sheetA.setRowView(rCont, 30 * 20);
+                                        sheetA.addCell(new Label(0, rCont, codigo_correlativo));
+                                        sheetA.addCell(new Label(1, rCont, especie));
+                                        sheetA.addCell(new Label(2, rCont, "1"));
+                                        sheetA.addCell(new Label(3, rCont, estado));
+                                        sheetA.addCell(new Label(4, rCont, precio_unitario));
+                                        sheetA.addCell(new Label(5, rCont, precio_total));
+                                        sheetA.addCell(new Label(6, rCont, fecha_recepcion));
+                                        sheetA.addCell(new Label(7, rCont, numero_factura));
+                                        sheetA.addCell(new Label(8, rCont, rut_proveedor));
+                                        sheetA.addCell(new Label(9, rCont, centro_de_costo));
+                                        sheetA.addCell(new Label(10, rCont, ubicacion_actual));
+                                        sheetA.addCell(new Label(11, rCont, observaciones));
+                                    } catch (RowsExceededException e) {
+                                        e.printStackTrace();
+                                    } catch (WriteException e) {
+                                        e.printStackTrace();
+                                    }
+                                    rCont++;
+                                }
                             }
                         }
                         try {
@@ -153,7 +203,21 @@ class daoExportExcel extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
+    public ArrayList<String> getUbi(DatabaseReference mDatabase){
+        ArrayList<String> ubicaciones = new ArrayList<>();
+        Query query = mDatabase.child("data").child("especies");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(int i = 1; i<dataSnapshot.getChildrenCount(); i++){
+                    String cont = String.valueOf(i);
+                    String auxUbi = String.valueOf(dataSnapshot.child(cont).child("ubicacion_actual").getValue(String.class));
+                    if(!ubicaciones.contains(auxUbi)){ubicaciones.add(auxUbi);}
+                }
+            }
+            public void onCancelled(@NonNull DatabaseError databaseError) {}});
+        return ubicaciones;
+    }
     public void copyFile(InputStream in, OutputStream out) throws IOException{
         byte[] buffer = new byte[1024];
         int read;
