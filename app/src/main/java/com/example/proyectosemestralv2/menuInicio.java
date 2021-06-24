0e933605d9a86adf483db1bc5672f42a660106d5
Cristobal Astudillo <labotsirc.1997@gmail.com>
2021-06-24 17:29:13 -0400

    Se dan de baja layouts sin uso dentro del proyecto.

--- app/src/main/java/com/example/proyectosemestralv2/menuInicio.java
+++ app/src/main/java/com/example/proyectosemestralv2/menuInicio.java
@@ -1,10 +1,11 @@
 package com.example.proyectosemestralv2;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class menuInicio extends AppCompatActivity implements View.OnClickListener {
 
@@ -89,7 +90,5 @@ public class menuInicio extends AppCompatActivity implements View.OnClickListene
                 break;
         }
     }
-//
-  //  public void onBackPressed(){
-    //}
+
 }
